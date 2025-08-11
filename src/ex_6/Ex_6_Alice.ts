8c9f8922c41fa9ab88ba1a55e897b6296a933448
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2025-08-11 22:43:59 +0200

    Updated ex 6

--- src/ex_6/Ex_6_Alice.ts
+++ src/ex_6/Ex_6_Alice.ts
@@ -2,9 +2,7 @@ import {
     CrolangP2PJs, 
     OnNewSocketMsgJsBuilder,
     BrokerConnectionAdditionalParametersJsBuilder,
-    CrolangNodeConnectionTargetsJs,
     CrolangNodeConnectionTargetsJsBuilder,
-    CrolangNodeCallbacksJs,
     CrolangNodeCallbacksJsBuilder
 } from 'crolang-p2p-node';
 import { ALICE_ID, BOB_ID, CAROL_ID, BROKER_ADDR } from '../Constants';
@@ -17,27 +15,26 @@ CrolangP2PJs.connectToBroker(
 ).then(() => {
     console.log(`Connected to Broker at ${BROKER_ADDR} as ${ALICE_ID}`);
 
-    CrolangP2PJs.connectToMultipleNodes(CrolangNodeConnectionTargetsJsBuilder.create()
-        .addTarget(BOB_ID, CrolangNodeCallbacksJsBuilder.create().setOnConnectionSuccess(node => {
-            const bob = CrolangP2PJs.getConnectedNode(BOB_ID);
-            if(bob !== null){
-                console.log("Node " + BOB_ID + " is connected");
-                bob.send("GREETINGS_CHANNEL", "Hello " + bob.id + "!");
-            }
-        }))
-        .addTarget(CAROL_ID, CrolangNodeCallbacksJsBuilder.create())
-        //TODO onConnectionAttemptConcluded, with code below inside
+    CrolangP2PJs.connectToMultipleNodes(
+        CrolangNodeConnectionTargetsJsBuilder.create()
+            .addTarget(BOB_ID, CrolangNodeCallbacksJsBuilder.create())
+            .addTarget(CAROL_ID, CrolangNodeCallbacksJsBuilder.create()),
+        () => {
+            CrolangP2PJs.getConnectedNode(BOB_ID)
+                .then(bob => {
+                    if (bob !== null) {
+                        console.log(`Node ${BOB_ID} is connected`);
+                        bob.send("GREETINGS_CHANNEL", "Hello " + bob.id + "!");
+                    }
+                });
+            CrolangP2PJs.getAllConnectedNodes()
+                .then(nodes => {
+                    const carol = nodes.find(node => node.id === CAROL_ID);
+                    if (carol !== undefined) {
+                        console.log(`Node ${CAROL_ID}` + " is connected");
+                        carol.send("GREETINGS_CHANNEL", "Hello " + carol.id + "!");
+                    }
+                });
+        }
     );
-
-    const bob = CrolangP2PJs.getConnectedNode(BOB_ID);
-    if(bob !== null){
-        console.log("Node " + BOB_ID + " is connected");
-        bob.send("GREETINGS_CHANNEL", "Hello " + bob.id + "!");
-    }
-
-    const carol = CrolangP2PJs.getAllConnectedNodes().find(node => node.id === CAROL_ID);
-    if(carol !== undefined){
-        console.log("Node " + CAROL_ID + " is connected");
-        carol.send("GREETINGS_CHANNEL", "Hello " + carol.id + "!");
-    }
 });
